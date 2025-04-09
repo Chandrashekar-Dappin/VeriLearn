86678e5d01f4167c6214531702510a25dd3f3b1d
Digital Sculpture <chandrashekardappin14@gmail.com>
2025-04-10 01:01:37 +0530

    Update mux.v

--- combinational circuits/2:1 mux/mux.v
+++ combinational circuits/2:1 mux/mux.v
@@ -1,1 +1,7 @@
-
+module mux_2_1( I0,I1,s,y);
+  input I0,I1,s; //input port declaration
+  output y;      //output port declaration
+  
+  assign y = ((~s)&I0) | (s&I1); //assign the logic
+  
+endmodule
