cf50d17eafe4c1ba7deff32ebaa63c3c601b2040
Digital Sculpture <chandrashekardappin14@gmail.com>
2025-04-10 01:17:15 +0530

    Update half_adder_tb.v

--- combinational circuits/half adder/half_adder_tb.v
+++ combinational circuits/half adder/half_adder_tb.v
@@ -1,1 +1,25 @@
+module half_adder_tb;
+  reg a,b;
+  wire s,c;
+  
+  half_adder dut(a,b,s,c);  //module instantiation
+  
+  initial begin
+    a=0; b=0;
+    #5 b=1;           // stimulus genaration
+    #5 a=1; b=0;
+    #5 b=1;
+    
 
+  end
+  
+  initial begin
+    $monitor("sim time = %0t, a = %0d, b = %0d, s = %0d ,c = %0d ",$time,a,b,s,c);
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,a,b,s,c);
+  end
+  
+endmodule
