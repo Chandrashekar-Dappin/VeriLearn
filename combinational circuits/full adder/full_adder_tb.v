5effd282796586218250a5dbb7c1df4754847435
Digital Sculpture <chandrashekardappin14@gmail.com>
2025-04-26 16:01:26 +0530

    Update full_adder_tb.v

--- combinational circuits/full adder/full_adder_tb.v
+++ combinational circuits/full adder/full_adder_tb.v
@@ -2,9 +2,9 @@ module full_adder_tb;
   reg a,b,c;
   wire s,cout;
 
-  full_adder dut(a,b,c,s,cout);
+  full_adder dut(a,b,c,s,cout);  // instantiating the design 
 
-  initial begin
+  initial begin           //generating stimulus
     a=0; b=0; c=0;
     #5 a=0; b=0; c=1;
     #5 a=0; b=1; c=0;
@@ -14,6 +14,15 @@ module full_adder_tb;
     #5 a=1; b=1; c=0;
     #5 a=1; b=1; c=1;
 
+  end
+
+  initial begin
+    $monitor("a=%0d, b=%0d, c=%0d, s=%0d, cout=%0d",a,b,c,s,cout);
+  end
+
+  initial begin
+    $dumpfile("dump.vcd");        // for analysing waveform
+    $dumpvars(1,a,b,c,s,cout);
   end
 
 endmodule
