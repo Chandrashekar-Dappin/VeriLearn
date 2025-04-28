8d8f835a34703eac67e30847178a3d279c494b93
Digital Sculpture <chandrashekardappin14@gmail.com>
2025-04-29 00:15:02 +0530

    Create counter_3bit_gradually_up_down.v

---
+++ counters/counter_3bit_gradually_up_down.v
@@ -0,0 +1,36 @@
+module counter_up_down_3bit(clk,rst,count);
+  input clk,rst;
+  output reg [2:0] count;
+  reg ctrl;
+  
+  always@(posedge clk)
+    begin
+      if(rst) begin
+        count<=0;
+        ctrl<=1;
+      end
+      
+      else 
+        if(ctrl)
+          if(count==7) begin
+            ctrl<=0;
+            count<=count-1;
+          end
+         
+          else
+            count<=count+1;
+      
+        
+        else
+          if(count==0) begin
+            ctrl<=1;
+            count<=count+1;
+          end
+        
+          else
+            count<=count-1;
+    end    
+
+  
+  
+endmodule
