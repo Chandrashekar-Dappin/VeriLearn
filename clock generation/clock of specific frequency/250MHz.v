8ac3792b35a470d92690043f0ce23d67eed5c93f
Digital Sculpture <chandrashekardappin14@gmail.com>
2025-04-28 22:15:08 +0530

    Create 250MHz.v

---
+++ clock generation/clock of specific frequency/250MHz.v
@@ -0,0 +1,20 @@
+module test;
+  reg clk;
+  real freq = 250;  // in MHz
+  real time_period;
+  
+  initial begin
+    clk=0;
+    time_period = 1000/freq;   //in ns
+    #100 $finish;
+  end
+  
+  always # (time_period/2) clk=~clk;
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1,test);
+  end
+  
+endmodule
+    
