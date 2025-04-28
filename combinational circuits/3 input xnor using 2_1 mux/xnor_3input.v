7cf47229d47f661b030ef1af532a074c88f725b8
Digital Sculpture <chandrashekardappin14@gmail.com>
2025-04-28 15:18:57 +0530

    Update xnor_3input.v

--- combinational circuits/3 input xnor using 2_1 mux/xnor_3input.v
+++ combinational circuits/3 input xnor using 2_1 mux/xnor_3input.v
@@ -3,8 +3,8 @@ module xnor_3input (a,b,c,y);
   output y;
   wire w1,w2;
 
-  mux m1 ({c,~c},b,w1);
-  mux m2 ({~c,c},b,w2);
-  mux m3({w2,w1},a,y);
+  mux_2_1 m1 ({c,~c},b,w1);
+  mux_2_1 m2 ({~c,c},b,w2);
+  mux_2_1 m3({w2,w1},a,y);
 
 endmodule
