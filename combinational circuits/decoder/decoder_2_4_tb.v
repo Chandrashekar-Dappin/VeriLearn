fb746a5bbc277159dd8790945057a2eaa1caf192
Digital Sculpture <chandrashekardappin14@gmail.com>
2025-04-28 12:36:50 +0530

    Update decoder_2_4_tb.v

--- combinational circuits/decoder/decoder_2_4_tb.v
+++ combinational circuits/decoder/decoder_2_4_tb.v
@@ -1,1 +1,23 @@
-
+module decoder_2_4_tb;
+  reg [1:0] I;
+  wire [3:0] D;
+  
+  decoder_2_4(I,D);
+  
+  initial begin
+    
+    $monitor("time=%0t , I=%0b, D=%0b",$time,I,D);
+    
+    I=2'b00; #10;
+    I=2'b01; #10;
+    I=2'b10; #10;
+    I=2'b11; #10;
+    
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1,I,D);
+  end
+  
+endmodule
