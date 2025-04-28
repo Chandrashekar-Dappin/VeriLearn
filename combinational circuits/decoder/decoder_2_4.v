e85fdf84eb8c1b60129da6ca02dd25004f071295
Digital Sculpture <chandrashekardappin14@gmail.com>
2025-04-28 12:36:04 +0530

    Update decoder_2_4.v

--- combinational circuits/decoder/decoder_2_4.v
+++ combinational circuits/decoder/decoder_2_4.v
@@ -1,1 +1,10 @@
-
+module decoder_2_4 (I,D);
+  input [1:0] I;
+  output [3:0] D;
+  
+  assign D[0] = (~I[1]) & (~I[0]);
+  assign D[1] = (~I[1]) & (I[0]);
+  assign D[2] = (I[1]) & (~I[0]);
+  assign D[3] = (I[1]) & (I[0]);
+  
+endmodule
