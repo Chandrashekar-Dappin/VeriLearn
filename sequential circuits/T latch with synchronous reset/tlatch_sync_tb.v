e7e438729c9f603bc5b634b4bb517b77614b4e0b
Digital Sculpture <chandrashekardappin14@gmail.com>
2025-04-28 19:28:20 +0530

    Create tlatch_sync_tb.v

---
+++ sequential circuits/T latch with synchronous reset/tlatch_sync_tb.v
@@ -0,0 +1,32 @@
+module test;
+  reg clk,rst,t;
+  wire q;
+  
+  tlatch dut(t,clk,rst,q);
+  
+  initial begin
+    clk=0;
+    rst=1;
+    #10 rst=0;
+    t=0;
+    #5 t=1;
+    #5 t=0;
+    #5 t=1;
+    #5 t=0;
+    #50 $finish;
+  end
+  
+  initial begin
+    $monitor("sim time=%0t, t=%0b, q=%0b",$time,t,q);
+  end
+  
+  always #5 clk=~clk;
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1,test);
+  end
+  
+endmodule
+    
+        
