7bc9940cbd19cfaad9dc4219d690d7766111e555
Digital Sculpture <chandrashekardappin14@gmail.com>
2025-04-28 23:03:23 +0530

    Create clk1_clk2.v

---
+++ clock generation/clk1_clk2.v
@@ -0,0 +1,21 @@
+
+module test;
+  reg clk1,clk2;
+  real time_period = 20;  //20ns
+  
+  initial begin
+    clk1=0;
+    clk2=0;
+    #100 $finish;
+  end
+  
+  always #(time_period/2) clk1=~clk1;
+  
+  always@(clk1)
+    #5 clk2=clk1;
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1,test); 
+  end
+endmodule
