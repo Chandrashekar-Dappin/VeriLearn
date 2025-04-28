61538beb9778e4143e2f67b88a7c5e3295ce1a6b
Digital Sculpture <chandrashekardappin14@gmail.com>
2025-04-28 23:26:40 +0530

    Create counter_down_3bit.v

---
+++ counters/counter_down_3bit.v
@@ -0,0 +1,16 @@
+module counter_up_3bit(clk,rst,count);
+  input clk,rst;
+  output [2:0] count;
+  reg [2:0] temp;
+  
+  always@(posedge clk)
+    begin
+      if(rst)
+        temp<=0;
+      else
+        temp<=temp-1;
+    end
+  
+  assign count = temp;
+  
+endmodule
