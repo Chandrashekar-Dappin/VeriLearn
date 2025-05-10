8e9b243230e6c6f5689eb726fca1fe440b8e1488
Digital Sculpture <chandrashekardappin14@gmail.com>
2025-05-10 16:37:10 +0530

    Create njumber_increment_right_aligned.v

---
+++ Examples/patterns/njumber_increment_right_aligned.v
@@ -0,0 +1,18 @@
+module numbers_decrement;
+  integer i,j;
+  
+  initial begin
+    for (i=1 ; i<6 ; i=i+1) begin
+      for(j=1; j<=6-i ; j=j+1) begin
+        $write("  ");
+      end
+      
+      for(j=1 ; j<=i ; j=j+1)begin
+        $write("%0d ",j);
+      end
+      
+      $display();
+    end
+    
+    end
+endmodule
