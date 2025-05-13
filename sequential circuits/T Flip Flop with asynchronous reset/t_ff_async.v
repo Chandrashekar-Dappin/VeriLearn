2ea9b05abbe323a9beae1690f37e16924d2a8060
Digital Sculpture <chandrashekardappin14@gmail.com>
2025-05-13 20:31:41 +0530

    Create t_ff_async.v

---
+++ sequential circuits/T Flip Flop with asynchronous reset/t_ff_async.v
@@ -0,0 +1,57 @@
+module tff_async(t,clk,rst,q);
+  input clk,rst,t;
+  output reg q;
+  reg temp;
+  
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+        temp<=0;
+      
+      else
+        if(clk)
+          if(t)
+            temp<=~temp;
+          else
+            temp<=~temp;
+      
+      q<=temp;       
+     
+    end
+  
+endmodule
+      
+
+module test;
+  reg clk,rst,t;
+  wire q;
+  
+  tff_async dut(t,clk,rst,q);
+  
+  initial begin
+    clk=0;
+    rst=1;
+    #10 rst=0;
+    t=0;
+    #5 t=1;
+    #5 t=0;
+    rst=1;
+    #6 rst=0;
+    #5 t=1;
+    #5 t=0;
+    #50 $finish;
+  end
+  
+  initial begin
+    $monitor("sim time=%0t, t=%0b, q=%0b",$time,t,q);
+  end
+  
+  always #5 clk=~clk;
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1,test);
+  end
+  
+endmodule
+    
