43036d147f2bed15a8db899cfe91d96b74519ee5
Digital Sculpture <chandrashekardappin14@gmail.com>
2025-07-19 22:47:22 +0530

    Create 10100100010000... inside 32 bit register.v

---
+++ Verilog Interview Questions/10100100010000... inside 32 bit register.v
@@ -0,0 +1,27 @@
+module pattern;
+  
+  reg [0:31] a;
+  
+  int count;
+  
+  initial begin
+    for(int i=1; i<10; i++) begin
+      
+      a[count] = 1'b1;
+      count++;
+      
+      for(int j=0; j<i; j++) begin
+        a[count] = 1'b0;
+        count++;
+      end
+      
+    end
+    
+    $display(" a = %b",a);
+    
+  end
+  
+endmodule
+
+//Output
+a = 10100100010000100000100000010000
